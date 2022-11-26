8cf4e7a7e890ebb5a78da8e4e609296a18819103
RudranshVerma <83331599+RudranshVerma@users.noreply.github.com>
2022-11-26 19:41:26 +0530

    commit

--- lib/auth/customer_signup.dart
+++ lib/auth/customer_signup.dart
@@ -8,6 +8,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import '../widgtes/auth_widgets.dart';
+
 import 'package:firebase_storage/firebase_storage.dart';
 
 class CustomerRegister extends StatefulWidget {
