4675c72a5b56b2334b11d63f889837f4a85ae6ee
Saarim Khan <saarimkhan515@gmail.com>
2023-02-19 18:41:00 +0530

    Payment Screen created and orders uploade stuff done

--- lib/customers_screens/customers_orders.dart
+++ lib/customers_screens/customers_orders.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_app/widgtes/appbar_widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../models/cust_order_model.dart';
 
 class CustomerOrders extends StatelessWidget {
   const CustomerOrders({super.key});
@@ -10,8 +14,44 @@ class CustomerOrders extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
-        title: const AppbarTitle(title: "CustomerOrders"),
+        title: const AppbarTitle(title: "Orders"),
         leading: const AppBarBackButton(),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('orders')
+            .where('cid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.data!.docs.isEmpty) {
+            return const Center(
+                child: Text(
+              'You have not \n\n active Orders!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.blueGrey,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Acme',
+                  letterSpacing: 1.5),
+            ));
+          }
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                return CustomerOrderModel(order: snapshot.data!.docs[index]);
+              });
+        },
       ),
     );
   }
